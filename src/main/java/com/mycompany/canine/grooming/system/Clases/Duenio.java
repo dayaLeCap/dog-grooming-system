b1c47a5ee30d16ed439cfb6019ce4ea3e78cd05e
Dayana <lemadayus@gmail.com>
2024-07-17 08:43:56 -0500

    class Duenio: atributes

---
+++ src/main/java/com/mycompany/canine/grooming/system/Clases/Duenio.java
@@ -0,0 +1,9 @@
+package com.mycompany.canine.grooming.system.Clases;
+
+public class Duenio {
+    //atributes
+    private int id_duenio;
+    private String nombre;
+    private String cellDuenio;
+    
+}
