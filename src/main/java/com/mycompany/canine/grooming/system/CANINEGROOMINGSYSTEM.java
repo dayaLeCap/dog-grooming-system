965845bd6b9106c6f6713fe4cbb873f9b0eb59b4
Dayana <lemadayus@gmail.com>
2024-07-17 08:18:37 -0500

    project initialization

---
+++ src/main/java/com/mycompany/canine/grooming/system/CANINEGROOMINGSYSTEM.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.canine.grooming.system;
+
+/**
+ *
+ * @author User
+ */
+public class CANINEGROOMINGSYSTEM {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
