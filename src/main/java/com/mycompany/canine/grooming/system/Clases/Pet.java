4fea5e1fd0cd53da170a6919fd329ea6d2f7408f
Dayana <lemadayus@gmail.com>
2024-07-17 08:58:33 -0500

    class Pet: generation of constructors, getters and

--- src/main/java/com/mycompany/canine/grooming/system/Clases/Pet.java
+++ src/main/java/com/mycompany/canine/grooming/system/Clases/Pet.java
@@ -11,4 +11,84 @@ public class Pet {
     private String special_attention;
     private String observations;
 
+    private Owner owner;
+
+    public Pet() {
+    }
+
+    public Pet(int customer_number, String name, String race, String color, String allergic, String special_attention, String observations, Owner owner) {
+        this.customer_number = customer_number;
+        this.name = name;
+        this.race = race;
+        this.color = color;
+        this.allergic = allergic;
+        this.special_attention = special_attention;
+        this.observations = observations;
+        this.owner = owner;
+    }
+
+    public int getCustomer_number() {
+        return customer_number;
+    }
+
+    public void setCustomer_number(int customer_number) {
+        this.customer_number = customer_number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getAllergic() {
+        return allergic;
+    }
+
+    public void setAllergic(String allergic) {
+        this.allergic = allergic;
+    }
+
+    public String getSpecial_attention() {
+        return special_attention;
+    }
+
+    public void setSpecial_attention(String special_attention) {
+        this.special_attention = special_attention;
+    }
+
+    public String getObservations() {
+        return observations;
+    }
+
+    public void setObservations(String observations) {
+        this.observations = observations;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
 }
