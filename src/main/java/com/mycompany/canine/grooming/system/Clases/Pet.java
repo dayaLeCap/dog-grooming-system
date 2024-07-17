cc2df334cc03149d1b854c25052815b5a0dcff67
Dayana <lemadayus@gmail.com>
2024-07-17 08:55:32 -0500

    class Pet: atributes

---
+++ src/main/java/com/mycompany/canine/grooming/system/Clases/Pet.java
@@ -0,0 +1,14 @@
+package com.mycompany.canine.grooming.system.Clases;
+
+public class Pet {
+
+    //atributes
+    private int customer_number;
+    private String name;
+    private String race;
+    private String color;
+    private String allergic;
+    private String special_attention;
+    private String observations;
+
+}
