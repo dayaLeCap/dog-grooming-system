f95a2e98773c86b3b171d53ef60bd259e0e24bbb
Dayana <lemadayus@gmail.com>
2024-07-17 08:59:44 -0500

    main example

--- src/main/java/com/mycompany/canine/grooming/system/mainExample.java
+++ src/main/java/com/mycompany/canine/grooming/system/mainExample.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.canine.grooming.system;
 
 import com.mycompany.canine.grooming.system.IGU.Dashboard;
 
-/**
- *
- * @author User
- */
-public class CANINEGROOMINGSYSTEM {
+public class mainExample {
 
     public static void main(String[] args) {
         Dashboard d = new Dashboard();
