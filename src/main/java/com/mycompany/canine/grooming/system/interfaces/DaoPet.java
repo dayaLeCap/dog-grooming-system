68b0eb7d3d27d669c62cbe54150731aa5624a571
sumativa <danielayumbo97@gmail.com>
2024-07-19 15:47:52 -0500

    Dao-pet interface: adding more methods even without functionality such as "modify both the pet and its owner", "obtain information about the pet" and "obtain information from the owner"

--- src/main/java/com/mycompany/canine/grooming/system/interfaces/DaoPet.java
+++ src/main/java/com/mycompany/canine/grooming/system/interfaces/DaoPet.java
@@ -8,4 +8,10 @@ public interface DaoPet {
     public void registerPet(Owner owner, Pet pet);
 
     public void deletePet(Pet pet, Owner owner);
+
+    public void modifyPetAndOwner(Owner owner, Pet pet);
+
+    public Pet getPetInformation(int idPet);
+
+    public Owner getOwnerInformation(int idOwner);
 }
